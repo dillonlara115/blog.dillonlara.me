b3cad7b43257b00eff17b6b08bed68f88c78e86a
Dillon <dillonlara115@gmail.com>
2021-02-17 13:48:47 -0700

    intial commit

---
+++ gridsome.server.js
@@ -0,0 +1,42 @@
+module.exports = function (api) {
+  api.createPages(async ({ graphql, createPage }) => {
+    // Setup our query to get the WordPress data
+    const query = `{
+      posts {
+        edges {
+          node {
+            title
+            slug
+            content
+            author {
+              node {
+                name
+              }
+            }
+          }
+        }
+      }
+    }`
+
+    // Execute the query
+    const queryResult = await graphql(query)
+
+    // Pull the data from the query into a variable thats easier to work with
+    let posts = queryResult.data.posts.edges.map(edge => edge.node)
+
+    console.log('posts', JSON.stringify(posts))
+
+    // For each post, generate a page using the context & template we created
+    posts.forEach(p => {
+      createPage({
+        path: `/blog/${p.slug}`,
+        component: './src/templates/Post.vue',
+        context: {
+          title: p.title,
+          content: p.content,
+          author: p.author.node.name
+        }
+      })
+    })
+  })
+}
---
+++ src/templates/Post.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <h1 v-html="$context.title"></h1>
+    <h2 v-html="$context.author"></h2>
+    <div v-html="$context.content"></div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: function () {
+    return {
+      title: this.$context.title
+    }
+  }
+}
+</script>
